ccbe0082bf7481e5c0410cc410d86b8af8e4952a
Gabrielle Germain <57801267+GAGER41@users.noreply.github.com>
2024-07-15 17:23:53 -0400

    monday meh

--- analysisPipeline/analysisFunctions.py
+++ analysisPipeline/analysisFunctions.py
@@ -4,52 +4,53 @@ from tkinter import filedialog
 from tkinter import *
 from tqdm import tqdm
 # from matplotlib import pyplot as plt
+import shutil
 
 
-def splitChannels(FLAG405:bool = 1, FLAG470:bool = 1, FLAG530:bool = 1, FLAG625:bool = 1, FLAG785:bool = 1, fin:int=100) ->tuple:
-    # folderPath:str,
-    """_summary_
+# def splitChannels(FLAG405:bool = 1, FLAG470:bool = 1, FLAG530:bool = 1, FLAG625:bool = 1, FLAG785:bool = 1, fin:int=100) ->tuple:
+#     # folderPath:str,
+#     """_summary_
 
-    Args:
-        folderPath (str): _description_
-        FLAG405 (bool, optional): _description_. Defaults to 1.
-        FLAG470 (bool, optional): _description_. Defaults to 1.
-        FLAG530 (bool, optional): _description_. Defaults to 1.
-        FLAG625 (bool, optional): _description_. Defaults to 1.
-        FLAG785 (bool, optional): _description_. Defaults to 1.
-        fin (int, optional): _description_. Defaults to 100.
+#     Args:
+#         folderPath (str): _description_
+#         FLAG405 (bool, optional): _description_. Defaults to 1.
+#         FLAG470 (bool, optional): _description_. Defaults to 1.
+#         FLAG530 (bool, optional): _description_. Defaults to 1.
+#         FLAG625 (bool, optional): _description_. Defaults to 1.
+#         FLAG785 (bool, optional): _description_. Defaults to 1.
+#         fin (int, optional): _description_. Defaults to 100.
 
-    Returns:
-        tuple: _description_
-    """
+#     Returns:
+#         tuple: _description_
+#     """
 
-    root = Tk()
-    root.withdraw()
-    folderPath = filedialog.askdirectory()
+#     root = Tk()
+#     root.withdraw()
+#     folderPath = filedialog.askdirectory()
 
-    FLAGS = {"FLAG405":FLAG405, "FLAG470":FLAG470, "FLAG530":FLAG530, "FLAG630":FLAG625, "FLAG785":FLAG785}
-    for flagName, flag in zip(FLAGS.keys, FLAGS.values):
-        if flag:
-            os.mkdir()
+#     FLAGS = {"FLAG405":FLAG405, "FLAG470":FLAG470, "FLAG530":FLAG530, "FLAG630":FLAG625, "FLAG785":FLAG785}
+#     for flagName, flag in zip(FLAGS.keys, FLAGS.values):
+#         if flag:
+#             os.mkdir()
 
 
-    filesList = os.listdir(folderPath)
-    filesPaths = []
-    for idx, file in enumerate(tqdm(filesList)):
-        filesPaths.append(folderPath + "\\" + file)
+#     filesList = os.listdir(folderPath)
+#     filesPaths = []
+#     for idx, file in enumerate(tqdm(filesList)):
+#         filesPaths.append(folderPath + "\\" + file)
 
-    filesPaths.sort(key=lambda x: os.path.getmtime(x))
-    filesPaths = filesPaths[1:]
+#     filesPaths.sort(key=lambda x: os.path.getmtime(x))
+#     filesPaths = filesPaths[1:]
 
-    fin = 100
+#     fin = 100
 
-    filesList405 = filesPaths[0:fin:5]
-    filesList470 = filesPaths[1:fin:5]
-    filesList530 = filesPaths[2:fin:5]
-    filesList625 = filesPaths[3:fin:5]
-    filesList785 = filesPaths[4:fin:5]
+#     filesList405 = filesPaths[0:fin:5]
+#     filesList470 = filesPaths[1:fin:5]
+#     filesList530 = filesPaths[2:fin:5]
+#     filesList625 = filesPaths[3:fin:5]
+#     filesList785 = filesPaths[4:fin:5]
 
-    return (filesList405, filesList470, filesList530, filesList625, filesList785)
+#     return (filesList405, filesList470, filesList530, filesList625, filesList785)
 
 
 def normalizeData(data:list, dim:int=3) -> list:
@@ -63,7 +64,7 @@ def normalizeData(data:list, dim:int=3) -> list:
         list: _description_
     """
     datamin = np.min(data, axis=dim-1)[..., np.newaxis]
-    datamax = np.max(data - datamin, axis=2)[..., np.newaxis]
+    datamax = np.max(data - datamin, axis=dim-1)[..., np.newaxis]
     datamax[np.where(datamax == 0)] = 1
     return ((data - datamin)/(datamax))
 
@@ -94,16 +95,18 @@ def deltaFoverF(data: list, dim:int=3) -> list:
     return (data - avg)/avg
 
 
-def oxygenation(greenData: list, redData: list) -> tuple:
+def oxygenation(greenData: list, redData: list, dim:int=3) -> tuple:
     """Generates a tuple that contains the variation of HbO and HbR concentrations over time
 
     Args:
         greenData (list): 530 nm absorption coefficient evolution in time
         redData (list): 625 nm absorption coefficient evolution in time
+        dim (int, optional): _description_. Defaults to 3.
 
     Returns:
         tuple: variation of HbR and HbO over time (delta c_HbR, delta c_HbO)
     """
+
     def absorptionCoefficientVariation(intensity:list, wavelength:int=530, dim:int=3) -> list:
         """Calculates the absorption coefficient variation for a specific wavelength
 
@@ -125,24 +128,28 @@ def oxygenation(greenData: list, redData: list) -> tuple:
         
         if dim == 3:
             iniIntens = intensity[:,:,0][..., np.newaxis]
+            iniIntens[np.where(iniIntens == 0)] = 1
+            mu = (-1/X)* np.log(intensity/iniIntens)
   
         elif dim == 2:
             iniIntens = intensity[:,0][..., np.newaxis]
+            iniIntens[np.where(iniIntens == 0)] = 1
+            mu = (-1/X)* np.log(intensity/iniIntens)
 
         elif dim == 1:
             iniIntens = intensity[0][..., np.newaxis]
+            iniIntens[np.where(iniIntens == 0)] = 1
+            mu = (-1/X)* np.log(intensity/iniIntens)
 
-        iniIntens[np.where(iniIntens == 0)] = 1
         # np.where(iniIntens == 0, 1, iniIntens)
         # print(np.shape(iniIntens))
-        with open('iniIntens{}.txt'.format(wavelength), 'w') as outfile:
-            for dataslice in iniIntens:
-                np.savetxt(outfile, dataslice)
-        mu = (-1/X)* np.log(intensity[:,:,1:]/iniIntens)
+        # with open('iniIntens{}.txt'.format(wavelength), 'w') as outfile:
+        #     for dataslice in iniIntens:
+        #         np.savetxt(outfile, dataslice)
         return mu
 
-    mu_530 = absorptionCoefficientVariation(greenData, 530)
-    mu_625 = absorptionCoefficientVariation(redData, 625)
+    mu_530 = absorptionCoefficientVariation(greenData, 530, dim)
+    mu_625 = absorptionCoefficientVariation(redData, 625, dim)
 
     eHbO_530 = 39956.8
     eHbR_530 = 39036.4
@@ -155,4 +162,4 @@ def oxygenation(greenData: list, redData: list) -> tuple:
     return (dc_HbR, dc_HbO)
 
 if __name__ == "__main__":
-    splitChannels()
+    pass
--- analysisPipeline/splitChannels.py
+++ analysisPipeline/splitChannels.py
@@ -1,14 +1,8 @@
 import os
 from tkinter import filedialog
 from tkinter import *
-# import cv2
-# import numpy as np
-# from matplotlib import pyplot as plt
-# import cmcrameri.cm as cmc
-# cmap = 'cmc.batlow'
-# import analysisFunctions as aF
+import shutil
 from tqdm import tqdm
-# from PIL import Image
 
 
 FLAG405 = 1
@@ -17,41 +11,31 @@ FLAG530 = 1
 FLAG625 = 1
 FLAG785 = 1
 
+FLAGS = {"FLAG405":FLAG405, "FLAG470":FLAG470, "FLAG530":FLAG530, "FLAG625":FLAG625, "FLAG785":FLAG785}
 
-# root = Tk()
-# root.withdraw()
-# folderPath = filedialog.askdirectory()
-folderPath = r"C:\Users\gabri\Desktop\testAnalyse"
+root = Tk()
+root.withdraw()
+folderPath = filedialog.askdirectory()
+# folderPath = r"C:\Users\gabri\Desktop\testAnalyse"
+
+
+print("Sorting data")
 
 filesPaths = [os.path.join(folderPath, file) for file in os.listdir(folderPath)]
 filesPaths.sort(key=lambda x: os.path.getmtime(x))
-# print(filesPaths)
 
-
-FLAGS = {"FLAG405":FLAG405, "FLAG470":FLAG470, "FLAG530":FLAG530, "FLAG630":FLAG625, "FLAG785":FLAG785}
-# for flagName, flag in zip(FLAGS.keys(), FLAGS.values()):
-#     # print(flagName, flag)
-#     if flag:
-#         os.makedirs(os.path.join(folderPath, flagName[4:7]))
-
-# fin = 100
 
 j = 0
-filesSplitList = []
 for flagName, flag in zip(FLAGS.keys(), FLAGS.values()):
     if flag:
-        filesChannel = filesPaths[j::5]
-        filesSplitList.append(filesChannel)
+        try:
+            os.makedirs(os.path.join(folderPath, flagName[4:7]))
+            print("Directory {} was created successfully".format(flagName[4:7]))
+        except OSError as error:
+            print("Directory {} already exists".format(flagName[4:7]))
+        filesChannel = filesPaths[j::sum(FLAGS.values())]
+
+        for file in tqdm(filesChannel):
+            splitPath = os.path.split(file)
+            shutil.move(file, os.path.join(splitPath[0], flagName[4:7], splitPath[1]))
         j += 1
-        print("filesChannel:")
-        print(filesChannel)
-
-print("filesSplitList:")
-print(filesSplitList)
-
-
-# filesList405 = 
-# filesList470 = filesPaths[1:fin:5]
-# filesList530 = filesPaths[2:fin:5]
-# filesList625 = filesPaths[3:fin:5]
-# filesList785 = filesPaths[4:fin:5]
