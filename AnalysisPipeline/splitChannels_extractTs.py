7109a0b2a42d43ea46cf22b30fd911a39e4e16e3
Frank0024 <francis.bernard.5@ulaval.ca>
2025-05-11 15:37:02 -0400

    Update splitChannels_extractTs.py

--- AnalysisPipeline/splitChannels_extractTs.py
+++ AnalysisPipeline/splitChannels_extractTs.py
@@ -151,4 +151,10 @@ if extractTs:
 
             # print(ts)
             # Save time stamps
-      
+            np.save(os.path.join(folderPath, flagName[4:7]) + 'ts.npy',ts)
+            print('Timestamps file was created for folder {}'.format(flagName[4:7]))
+
+
+#%% if GUI
+# if __name__ == '__main__':
+#     window.mainloop()
